df096e33c8e8ff2b293556f3629be52aa24a5ef3
Jonny Powell <jonnyowenpowell@gmail.com>
2021-12-25 12:01:38 +0000

    Moved level data to levels file

---
+++ levels.py
@@ -0,0 +1,19 @@
+from colours import Colour
+from tube import Tube
+
+level_105 = [
+    Tube([Colour.DARK_BLUE, Colour.DARK_GREEN, Colour.LIGHT_BLUE, Colour.LIGHT_BLUE]),
+    Tube([Colour.PURPLE, Colour.PINK, Colour.LIGHT_GREEN, Colour.GREY]),
+    Tube([Colour.ORANGE, Colour.PURPLE, Colour.RED, Colour.BROWN]),
+    Tube([Colour.ORANGE, Colour.PINK, Colour.RED, Colour.ORANGE]),
+    Tube([Colour.DARK_GREEN, Colour.RED, Colour.YELLOW, Colour.DARK_BLUE]),
+    Tube([Colour.YELLOW, Colour.DARK_GREEN, Colour.BROWN, Colour.DARK_GREEN]),
+    Tube([Colour.BROWN, Colour.PURPLE, Colour.RED, Colour.NEON_GREEN]),
+    Tube([Colour.NEON_GREEN, Colour.PURPLE, Colour.PINK, Colour.NEON_GREEN]),
+    Tube([Colour.LIGHT_GREEN, Colour.GREY, Colour.LIGHT_BLUE, Colour.DARK_BLUE]),
+    Tube([Colour.BROWN, Colour.YELLOW, Colour.GREY, Colour.LIGHT_GREEN]),
+    Tube([Colour.GREY, Colour.YELLOW, Colour.NEON_GREEN, Colour.DARK_BLUE]),
+    Tube([Colour.LIGHT_GREEN, Colour.LIGHT_BLUE, Colour.PINK, Colour.ORANGE]),
+    Tube(),
+    Tube(),
+]
